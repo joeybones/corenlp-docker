4a1fae581012fccef06b595a3dbfa35fc64139ef
Joe Hart <joe.hart@nytimes.com>
2018-04-15 16:55:21 -0400

    Proper arg format

--- corenlp.sh
+++ corenlp.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd /opt/corenlp/src
-exec java -mx10g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -timeout=300000 -annotators "tokenize,ssplit,pos,lemma,ner,parse,depparse,quote,mention,natlog,dcoref,openie"
-
+exec java -mx10g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -timeout 300000 -annotators "tokenize,ssplit,pos,lemma,ner,parse,depparse,quote,mention,natlog,dcoref,openie"
